3f3344b8d8a5621bc919ba4a7d910ac68ee24549
Ben <benoit.hagenbourger@gmail.com>
2016-08-04 00:01:22 +0200

    Add title content and theme for container and numbers

--- simplecountdown.js
+++ simplecountdown.js
@@ -14,16 +14,24 @@
     }
    },
    themes: {
-     raw: { 
-       brick:
-         ".brick{" + 
-    	   "padding: 10px;" + 
-    	   "display:inline-block;" +  
-         "}",
-       legend: 
-         ".legend{" + 
-    	  "display: block;" +
-         "}" 
+     raw: {
+       content: {
+         title: "My countdown"
+       },
+       style: { 
+         container: "",
+         title: "",
+         brick:
+           ".brick{" + 
+    	     "padding: 10px;" + 
+    	     "display:inline-block;" +  
+           "}",
+         number: "",
+         legend: 
+           ".legend{" + 
+    	    "display: block;" +
+           "}"
+       } 
      }
    },
    addTheme: function (newTheme) {
@@ -34,20 +42,21 @@
      }
    },
    loadCSS: function(theme){
-    theme = theme ? theme : "raw";
     var cssDiv = document.createElement('div');
-    cssDiv.innerHTML = '<style>' + this.themes[theme].brick + this.themes[theme].legend + '</style>';
+    cssDiv.innerHTML = '<style>' + this.themes[theme].style.container + this.themes[theme].style.brick + this.themes[theme].style.number + this.themes[theme].style.legend + '</style>';
     document.getElementsByTagName('head')[0].appendChild(cssDiv.childNodes[0]);
    },
    display: function(container, deadline, theme){
+    theme = theme ? theme : "raw";
     this.loadCSS(theme);
     var t = this.computeTimeRemaining(deadline);
-    document.getElementById(container).innerHTML = '<div class="brick">' 
-+ t.days + '<span class="legend">Days</span></div><div class="brick">' + t.hours + '<span class="legend">Hours</span></div><div class="brick">' + t.minutes + '<span class="legend">Minutes</span></div><div class="brick">' + t.seconds + '<span class="legend">Seconds</span></div>';
+    document.getElementById(container).className += " container";
+    document.getElementById(container).innerHTML = '<div class="title">' + this.themes[theme].content.title + '</div><div class="brick"><span class="number">' 
++ t.days + '</span><span class="legend">Days</span></div><div class="brick"><span class="number">' + t.hours + '</span><span class="legend">Hours</span></div><div class="brick"><span class="number">' + t.minutes + '</span><span class="legend">Minutes</span></div><div class="brick"><span class="number">' + t.seconds + '</span><span class="legend">Seconds</span></div>';
    },
    autoDisplay: function(container, deadline, theme){
      this.display(container, deadline, theme);
-     setInterval(function(){SimpleCountdown.display(container, deadline, theme);}, 1000);
+     setInterval(function(){SimpleCountdown.display(container, deadline, theme);}, 100000000);
    }
  };
 })();
